ee77551f876cc8ffad4e551a70e0f7461d0a262a
Hemal99 <h.githmin@gmail.com>
2022-06-25 11:53:39 +0530

    update

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { slides } = require("./carousel");
+const cors = require("cors");
+
 
 let app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
