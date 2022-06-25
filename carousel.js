85c27b06779f58608c4e38ce0a01c30dd7e0ccc7
Hemal99 <h.githmin@gmail.com>
2022-06-25 15:28:39 +0530

    updated

--- carousel.js
+++ carousel.js
@@ -1,62 +1,62 @@
 const slides = [
   {
     image: "https://picsum.photos/id/1/600/400",
-    title: "Strong mask",
+    title: "Image 1",
     subTitle: "Secondary text",
   },
   {
     image: "https://picsum.photos/id/237/600/400",
-    title: "Strong mask",
+    title: "Image 2",
     subTitle: "Secondary text",
   },
   {
     image: "https://picsum.photos/id/2/600/400",
-    title: "Strong mask",
+    title: "Image 3",
     subTitle: "Secondary text",
   },
   {
     image: "https://picsum.photos/id/3/600/400",
-    title: "Strong mask",
+    title: "Image 4",
     subTitle: "Secondary text",
   },
   {
     image: "https://picsum.photos/id/237/600/400",
-    title: "Strong mask",
+    title: "Image 5",
     subTitle: "Secondary text",
   },
   {
     image: "https://picsum.photos/id/4/600/400",
-    title: "Strong mask",
+    title: "Image 6",
     subTitle: "Secondary text",
   },
   {
     image: "https://picsum.photos/id/5/600/400",
-    title: "Strong mask",
+    title: "Image 7",
     subTitle: "Secondary text",
   },
   {
     image: "https://picsum.photos/id/6/600/400",
-    title: "Strong mask",
+    title: "Image 8",
     subTitle: "Secondary text",
   },
   {
     image: "https://picsum.photos/id/1/600/400",
-    title: "Strong mask",
+    title: "Image 9",
     subTitle: "Secondary text",
   },
   {
     image: "https://picsum.photos/id/9/600/400",
-    title: "Strong mask",
+    title: "Image 10",
     subTitle: "Secondary text",
   },
   {
     image: "https://picsum.photos/id/7/600/400",
-    title: "Strong mask",
+    title: "Image 12",
     subTitle: "Secondary text",
   },
   {
     image: "https://picsum.photos/id/8/600/400",
-    title: "Strong mask",
+    title: "Image 13",
     subTitle: "Secondary text",
   },
 ];
